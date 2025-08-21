d31c2646aca26017ced8cefa6edd5781d2e263a6
zoltanzsak <zoltanzsak.dev@gmail.com>
2025-08-21 18:42:22 +0200

    feat: redirect to game page after button click

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,65 +1,68 @@
 import clsx from 'clsx';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 type WelcomeScreenStep = 'enter-name' | 'start-game';
 
 export const Welcome = () => {
-  const [nameInputValue, setNameInputValue] = useState<string>('');
-  const [step, setStep] = useState<WelcomeScreenStep>('enter-name');
+    const [nameInputValue, setNameInputValue] = useState<string>('');
+    const [step, setStep] = useState<WelcomeScreenStep>('enter-name');
 
-  const buttonEnabled = !!nameInputValue.trim();
-  const inputLabel = step === 'enter-name' ? 'Name:' : 'Hello';
+    const navigate = useNavigate();
 
-  const handleBlur = () => {
-    const name = nameInputValue.trim();
-    if (name) {
-      setStep('start-game');
-    } else {
-      setStep('enter-name');
-    }
-  };
+    const buttonEnabled = !!nameInputValue.trim();
+    const inputLabel = step === 'enter-name' ? 'Name:' : 'Hello';
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    const key = e.key;
-    if (key === 'Enter' && buttonEnabled) {
-      handlePlay();
-    }
-  };
+    const handleBlur = () => {
+        const name = nameInputValue.trim();
+        if (name) {
+            setStep('start-game');
+        } else {
+            setStep('enter-name');
+        }
+    };
 
-  const handlePlay = () => {
-    if (buttonEnabled) {
-      console.log(`${nameInputValue} Started the game!`); // TODO
-    }
-  };
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        const key = e.key;
+        if (key === 'Enter' && buttonEnabled) {
+            handlePlay();
+        }
+    };
 
-  return (
-    <section className="mx-auto flex h-svh w-full flex-col justify-between py-8 md:w-1/2 lg:w-1/3">
-      <div className="flex flex-col gap-2">
-        <h1 className="text-center">Click The Fox! Game</h1>
-        <div className="grid w-full grid-cols-2 items-center [&>*]:text-xl">
-          <label className="mr-2 text-right" htmlFor="name-input">
-            {inputLabel}
-          </label>
-          <input
-            id="name-input"
-            className={clsx(
-              'w-full resize-none rounded-lg p-1 px-2 focus:outline-yellow-600',
-              step === 'enter-name' && 'yellow-border',
-            )}
-            value={nameInputValue}
-            onChange={(e) => setNameInputValue(e.target.value)}
-            onBlur={handleBlur}
-            onKeyDown={handleKeyDown}
-          />
-        </div>
-      </div>
-      <button
-        disabled={!buttonEnabled}
-        onClick={handlePlay}
-        className="yellow-border rounded-lg bg-yellow-600 p-2 px-4 text-white disabled:bg-white disabled:text-black"
-      >
-        PLAY!
-      </button>
-    </section>
-  );
+    const handlePlay = () => {
+        if (buttonEnabled) {
+            navigate('/game');
+        }
+    };
+
+    return (
+        <section className="mx-auto flex h-svh w-full flex-col justify-between py-8 md:w-1/2 lg:w-1/3">
+            <div className="flex flex-col gap-2">
+                <h1 className="text-center">Click The Fox! Game</h1>
+                <div className="grid w-full grid-cols-2 items-center [&>*]:text-xl">
+                    <label className="mr-2 text-right" htmlFor="name-input">
+                        {inputLabel}
+                    </label>
+                    <input
+                        id="name-input"
+                        className={clsx(
+                            'w-full resize-none rounded-lg p-1 px-2 focus:outline-yellow-600',
+                            step === 'enter-name' && 'yellow-border',
+                        )}
+                        value={nameInputValue}
+                        onChange={(e) => setNameInputValue(e.target.value)}
+                        onBlur={handleBlur}
+                        onKeyDown={handleKeyDown}
+                    />
+                </div>
+            </div>
+            <button
+                disabled={!buttonEnabled}
+                onClick={handlePlay}
+                className="yellow-border rounded-lg bg-yellow-600 p-2 px-4 text-white disabled:bg-white disabled:text-black"
+            >
+                PLAY!
+            </button>
+        </section>
+    );
 };
