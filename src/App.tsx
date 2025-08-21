690f1fa46fcb055e2262b237d42bf58ed1a0bf0d
zoltanzsak <zoltanzsak.dev@gmail.com>
2025-08-21 07:15:42 +0200

    feat: implement welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <div className="bg-red-400">Hello world</div>;
-}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Welcome } from './pages/welcome';
+import { Game } from './pages/game';
+import { ScoreBoard } from './pages/scoreboard';
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/score-board" element={<ScoreBoard />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="*" element={<Welcome />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/scoreboard.tsx
@@ -0,0 +1,3 @@
+export const ScoreBoard = () => {
+  return <>scoreboard</>;
+};
