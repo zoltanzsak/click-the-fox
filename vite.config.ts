12bef651323e96a8bf50078071a2d2a2b8615181
zoltanzsak <zoltanzsak.dev@gmail.com>
2025-08-24 12:20:48 +0200

    chore: fix vitest-related type issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import type { UserConfig } from 'vitest/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const config: UserConfig = {
     plugins: [react(), tailwindcss()],
     test: {
         globals: true,
         environment: 'jsdom',
         setupFiles: './src/tests/setup.ts',
     },
-});
+};
+
+export default defineConfig(config);
